ebbe435462b27cf4b6d79abbe58d48c1745b90cf
Zafarbek Yunusov <zyunusov30@gmail.com>
2025-03-17 17:34:40 +0500

    setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>React is working with Django!</h1>
+      <p>This is a test page</p>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+});
